614df23e3147568622be1c5d24029cc58007336e
Ioannis Tsagkatakis <tsagatakis@protonmail.com>
2022-04-09 23:40:54 +0300

    Add clock

--- configs/devilspie2/lua/desklets.lua
+++ configs/devilspie2/lua/desklets.lua
@@ -9,18 +9,25 @@ end
 
 local screen_w,screen_h = get_screen_size()
 local t_width = 540
-local t_height = 700 
+local t_height = 900 
+local cl_height = 200
+local w_height = 640
 
 if (get_window_class()=="rg.wezfurlong.wezweather") then
     make_widget()
-    set_window_size(t_width, t_height)
-    set_window_position2(screen_w - t_width - 10 , 10)
+    set_window_size(t_width, w_height)
+    set_window_position2(screen_w - t_width - 10 , 30 + cl_height)
 end
 
+if (get_window_class()=="rg.wezfurlong.wezclock") then
+    make_widget()
+    set_window_size(t_width, cl_height)
+    set_window_position2(screen_w - t_width - 10 , 15)
+end
 
 if (get_window_class()=="rg.wezfurlong.wezres") then
     make_widget()
-    set_window_size(t_width, t_height)
+    set_window_size(t_width, cl_height + w_height + 25)
     set_window_position2(screen_w - 2 * t_width - 20 , 10)
 end
 
@@ -29,8 +36,8 @@ end
 if (get_window_class()=="rg.wezfurlong.wezlog") then
     make_widget()
     local width = 2 * t_width + 10
-    local height = screen_h -  t_height
-    set_window_size(width, height)
-    set_window_position2(screen_w - width - 10 , screen_h - height)
+    local height = screen_h -  cl_height - w_height -10
+    set_window_size(width, height + 12) 
+    set_window_position2(screen_w - width - 10 , screen_h - height +10)
 end
 
