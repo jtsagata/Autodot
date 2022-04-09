f3f9de354cb50ad49db49990b9771baa73c2fb3a
Ioannis Tsagkatakis <tsagatakis@protonmail.com>
2022-04-09 22:30:07 +0300

    Pi Hole

--- configs/wterm/wez_widget.lua
+++ configs/wterm/wez_widget.lua
@@ -4,14 +4,14 @@ return {
   enable_tab_bar = false,
   enable_scroll_bar = false,
   window_decorations = "NONE",
-  default_cursor_style="SteadyUnderline",
+  default_cursor_style="SteadyBar",
   window_background_opacity = 0.0,
   keys = {
     {key="c", mods="CTRL", action="Nop"},
   },
   colors = {
-    cursor_bg = "black",
-    cursor_fg = "black",
-    cursor_border = "black",
+    cursor_bg = "none",
+    cursor_fg = "none",
+    cursor_border = "none",
   }
 }
