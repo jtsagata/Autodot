b8d55a9c747f9cf8c3edc3cae5abb919dcc1a44f
Ioannis Tsagkatakis <tsagatakis@protonmail.com>
2022-04-10 00:57:02 +0300

    Grandient

--- configs/wterm/wezterm.lua
+++ configs/wterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
   bold_brightens_ansi_colors = true,
-  window_background_opacity = 0.9,
+  window_background_opacity = 0.6,
   default_cursor_style = "BlinkingUnderline",
   font_size = 10,
   force_reverse_video_cursor = true,
@@ -19,5 +19,5 @@ return {
     },
     blend = "Rgb",
   },
-  text_background_opacity = 0.8,
+  text_background_opacity = 0.95,
 }
